ef7376d619b5b8a0fcdc463aaecb745f087bbf67
Carlos Galindo <carlosxgalindo@gmail.com>
2023-06-11 10:08:29 -0600

    9

---
+++ 9.py
@@ -0,0 +1,11 @@
+# Problem 9
+def sum_of_primes(n):
+    sieve = [True] * n
+    sieve[0] = sieve[1] = False
+    for i, is_prime in enumerate(sieve):
+        if is_prime:
+            for j in range(i*i, n, i):
+                sieve[j] = False
+    primes = [x for x in range(2, n) if sieve[x]]
+    return sum(primes)
+print(sum_of_primes(2000000))
