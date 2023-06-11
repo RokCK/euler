25b097ec7fc0b2ea49e779908cf8b399f63d901b
Carlos Galindo <carlosxgalindo@gmail.com>
2023-06-11 10:12:12 -0600

    25

---
+++ 25.py
@@ -0,0 +1,9 @@
+# Problem 25
+def fibonacci_index(digits):
+    a, b = 1, 1
+    index = 2
+    while len(str(b)) < digits:
+        a, b = b, a + b
+        index += 1
+    return index
+print(fibonacci_index(1000))
