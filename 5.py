18c1c6ff31f4b7e3de0e2d2a2d6c1283aacc60c8
Carlos Galindo <carlosxgalindo@gmail.com>
2023-06-11 01:52:02 -0600

    5

---
+++ 5.py
@@ -0,0 +1,8 @@
+# Problem 5
+import math
+def smallest_multiple(n):
+    result = 1
+    for i in range(1, n + 1):
+        result = result * i // math.gcd(result, i)
+    return result
+print(smallest_multiple(20))
