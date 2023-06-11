2236ab1e373a34c9cef397a7b567fede874d6f60
Carlos Galindo <carlosxgalindo@gmail.com>
2023-06-11 01:28:51 -0600

    2

---
+++ 2.py
@@ -0,0 +1,10 @@
+# Problem 2
+def sum_of_even_fibonacci(max_value):
+    a, b = 0, 1
+    s = 0 
+    while a <= max_value: 
+        if a % 2 == 0:
+            s += a
+        a, b = b, a + b 
+    return s
+print(sum_of_even_fibonacci(4000000))
