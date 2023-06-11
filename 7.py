b7c3decccb0c1a139e7aa3bed1b678368eb06016
Carlos Galindo <carlosxgalindo@gmail.com>
2023-06-11 09:59:25 -0600

    7

---
+++ 7.py
@@ -0,0 +1,13 @@
+# Problem 7
+def nth_prime(n):
+    upper_limit = int(n * (math.log(n) + math.log(math.log(n)))) + 1
+    sieve = [True] * upper_limit
+    sieve[0] = sieve[1] = False
+    for i, is_prime in enumerate(sieve):
+        if is_prime:
+            for j in range(i*i, upper_limit, i):
+                sieve[j] = False
+    primes = [x for x in range(2, upper_limit) if sieve[x]]
+    return primes[n-1]
+import math
+print(nth_prime(10001))
