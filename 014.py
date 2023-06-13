e9efa3664cc9a6bae1d16324ca8b3abd8cfb504d
Carlos Galindo <carlosxgalindo@gmail.com>
2023-06-13 00:02:51 -0600

    14

---
+++ 014.py
@@ -0,0 +1,17 @@
+# Problem 14
+
+def collatz(n):
+    while n != 1:
+        yield n
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+    yield 1
+longest_chain = (0, 0)
+for i in range(1, 1000000):
+    length = len(list(collatz(i)))
+    if length > longest_chain[1]:
+        longest_chain = (i, length)
+
+print(longest_chain[0])
