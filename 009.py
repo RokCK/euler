bf0a998ad21991015d8eef6ef4d414786e78172e
Carlos Galindo <carlosxgalindo@gmail.com>
2023-06-12 23:18:34 -0600

    009

---
+++ 009.py
@@ -0,0 +1,9 @@
+# Problem 9
+def pythagorean_triplet(n):
+    for a in range(1, n):
+        for b in range(a, n):
+            c = n - a - b
+            if a * a + b * b == c * c:
+                return a * b * c
+    return -1
+print(pythagorean_triplet(1000))
