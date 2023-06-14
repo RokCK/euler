d2841261a446ed71804e2bb557d5b7a1fc6fe1fd
Carlos Galindo <carlosxgalindo@gmail.com>
2023-06-14 00:38:10 -0600

    019

---
+++ 019.py
@@ -0,0 +1,11 @@
+# Problem 19
+import datetime
+
+sundays = 0
+
+for year in range(1901, 2001):
+    for month in range(1, 13):
+        if datetime.datetime(year, month, 1).weekday() == 6:
+            sundays += 1
+
+print(sundays)
