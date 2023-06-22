b62f6889b4988b7a9e2db347d501733047ee3908
Carlos Galindo <carlosxgalindo@gmail.com>
2023-06-21 23:26:58 -0600

    022

---
+++ 022.py
@@ -0,0 +1,13 @@
+# Problem 22
+
+def name_scores(filename):
+    with open(filename, 'r') as f:
+        names = f.read().replace('"', '').split(',')
+        names.sort()
+    total_score = 0
+    for i, name in enumerate(names, start=1):
+        name_score = sum(ord(char) - ord('A') + 1 for char in name)
+        total_score += i * name_score
+    return total_score
+
+print(name_scores('022_names.txt'))
